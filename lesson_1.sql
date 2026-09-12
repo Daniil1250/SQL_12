d4360b18691a0119db0922b4ef9ba4ee7d6b7a86
Daniil1250 <145423259+Daniil1250@users.noreply.github.com>
2026-09-12 20:16:50 +0300

    Update lesson_1.sql

--- lesson_1.sql
+++ lesson_1.sql
@@ -12,3 +12,37 @@ SELECT * FROM users;
 SELECT price FROM users;
 
 drop table users;
+
+-----------------------------------------------------------------------
+CREATE TABLE Trip (
+  id INT(12),
+  company INT(12),
+  plance VARCHAR(90),
+  town_from VARCHAR(78),
+  town_to VARCHAR(78),
+  town_out DATETIME,
+  time_in DATETIME
+);
+
+
+CREATE TABLE Company (
+  id INT(12),
+  name VARCHAR(34),
+  age INT(12)
+);
+
+
+INSERT INTO Company VALUES (12, 'Петя', 45);
+INSERT INTO Company VALUES (12, 'Иван', 18);
+INSERT INTO Company VALUES (12, 'Женя', 17);
+INSERT INTO Company VALUES (14, 'Вика', 15);
+
+SELECT * FROM Company;
+SELECT name, age FROM Company where age > 18;
+
+DROP TABLE Company;
+DROP TABLE Trip;
+
+
+
+
